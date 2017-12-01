ab19997d7b8669b25b38c039ebd6295d82bf9638
Devin Miller <email": null>
2017-11-30 17:07:35 -0800

    Create very basic views for ListItemsController#new and #index. Controllers work, to add items to list.

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -5,11 +5,18 @@ class ListItemsController < ApplicationController
   end
 
   def create
-    @list_item = ListItem.find_or_create_by(keywords: params[:keywords])
+
+    @list_item = ListItem.find_or_create_by(list_item_param)
     redirect_to list_items_path
   end
 
   def index
-    @list_items = ListItems.all
+    @list_items = ListItem.all
+  end
+
+  private
+
+  def list_item_param
+    params.require(:list_item).permit(:keywords)
   end
 end
