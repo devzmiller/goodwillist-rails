0c0a5e27b006eb8ee42cd85ab4976fba526ec272
Devin Miller <email": null>
2017-11-30 17:45:00 -0800

    Add bit to ListItem index view to show 'Available items' if anything matching the keywords exist. Refactor the find by keywords method to make it an instance method

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,6 +1,6 @@
 class ListItem < ApplicationRecord
 
-  def self.find_items_by_keyword(keyword)
-    InventoryItem.where("title like ?", '%' + keyword + '%')
+  def find_items_by_keyword
+    InventoryItem.where("title like ?", '%' + self.keywords + '%')
   end
 end
