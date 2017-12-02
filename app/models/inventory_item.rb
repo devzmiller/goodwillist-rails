a445ccc5a0dcb6eafde3549d0546584811318178
Devin Miller <email": null>
2017-12-01 17:55:50 -0800

    Add notes to InventoryItem model about getting API responses.

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,5 +1,10 @@
 class InventoryItem < ApplicationRecord
 
+  # This URL gets all the results up to a specific end date. Since you can only get 10k results at a time, this will be necessary to get the entire inventory. Will also need to specify EndTimeFrom.
+  # Need to handle pagination of response. 100 results per page. This request for everything ending up to three days ahead had 75 pages.
+  # https://rossta.net/blog/paginated-resources-in-ruby.html for info on handling pagination.
+  # "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsIneBayStores&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=#{ENV['EBAY_ID']}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&itemFilter.name=EndTimeTo&itemFilter.value=2017-12-04T19:09:02.768Z&storeName=seattlegoodwillbooks"
+
   def self.get_ebay_inventory
     url = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsIneBayStores&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=#{ENV['EBAY_ID']}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&storeName=seattlegoodwillbooks&paginationInput.entriesPerPage=100"
     response = RestClient.get(url)
